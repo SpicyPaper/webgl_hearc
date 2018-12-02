6967fef8f42950b77121430482799a4567d3ea00
Bastien Wermeille <bastien.wermeille@gmail.com>
2018-12-02 12:00:16 +0100

    Improve halo with 2d periodic perlin noise

--- labo2/js/shaders/sun.fragment.js
+++ labo2/js/shaders/sun.fragment.js
@@ -143,6 +143,120 @@ vec3 colorFromTextures(vec3 L, float oppZ, float newX, float newY, vec4 rotation
  return finalColor;
 }
 
+//
+// GLSL textureless classic 2D noise "cnoise",
+// with an RSL-style periodic variant "pnoise".
+// Author:  Stefan Gustavson (stefan.gustavson@liu.se)
+// Version: 2011-08-22
+//
+// Many thanks to Ian McEwan of Ashima Arts for the
+// ideas for permutation and gradient selection.
+//
+// Copyright (c) 2011 Stefan Gustavson. All rights reserved.
+// Distributed under the MIT license. See LICENSE file.
+// https://github.com/stegu/webgl-noise
+//
+
+vec4 mod289(vec4 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x)
+{
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec2 fade(vec2 t) {
+  return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// Classic Perlin noise
+float cnoise(vec2 P)
+{
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod289(Pi); // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+
+  vec4 i = permute(permute(ix) + iy);
+
+  vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0 ;
+  vec4 gy = abs(gx) - 0.5 ;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+
+  vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+
+// Classic Perlin noise, periodic variant
+float pnoise(vec2 P, vec2 rep)
+{
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, rep.xyxy); // To create noise with explicit period
+  Pi = mod289(Pi);        // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+
+  vec4 i = permute(permute(ix) + iy);
+
+  vec4 gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0 ;
+  vec4 gy = abs(gx) - 0.5 ;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+
+  vec4 norm = taylorInvSqrt(vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+
 void main(void) {
  //Calculates new x based on z offset
  float newX = vPosition.x * iZ / vPosition.w;
@@ -213,20 +327,33 @@ void main(void) {
 
      if (dist <= 2.0) {
        // iGlobalTime
-       float angle = atan(newY,newX)+iGlobalTime/2000.0;
-       float luminosity = sin(angle*1.0)+sin(angle*2.0)+cos(angle*3.0)+sin(angle*4.0);
+       float angle = atan(newY,newX)+M_PI;//+iGlobalTime/100000.0;
+       float angle2 = angle + iGlobalTime/10000.0;
+       float luminosity = sin(angle2*1.0)+sin(angle2*2.0)+cos(angle2*3.0)+sin(angle2*4.0);
+
+       //float luminosity2 = noise(vec2(sin(angle/2.)*3.,iGlobalTime/1000.0))*2.0;
+       float luminosity2 = pnoise(vec2(angle/M_PI*5.0,iGlobalTime/2000.0),vec2(10.0,250.0))*2.0;
+
+       float luminosity3 = (luminosity2+1.0)/2.0*0.7+0.3;
+       luminosity2 = (luminosity2+1.0)/2.0*0.5+0.5;
 
        vec2 normalized = normalize(vec2(newX,newY));
        rotationVector = rotYMatrix * rotXMatrix * vec4(normalized.x, normalized.y, 0.01, 1.0);
-       vec4 textureColor = vec4(colorFromTextures(L, planetPosition.z-0.1, normalized.x, normalized.y, rotationVector), 1.0);
+       vec4 textureColor = vec4(colorFromTextures(L, planetPosition.z, planetPosition.x, planetPosition.y, rotationVector), 1.0);
 
-       //finalColor += (luminosity+4.0)/8.0 * vec4(1.0, 1.0, 0.0, 0.8) / pow(abs(dist), 5.0);
-       finalColor += (luminosity+4.0)/8.0 * textureColor / pow(abs(dist), 5.0);
+       //finalColor += luminosity2 * vec4(1.0, 1.0, 1.0, 0.0);
+
+       // Final add halo
+       //float intensity = (textureColor.x + textureColor.y)/2.0;
+       //finalColor += (luminosity+4.0)/8.0 * vec4(1.0, 1.0, 0.0, 0.8) / pow(abs(dist), 5.0) / 2.0;
+       // finalColor += (luminosity+4.0)/8.0 * textureColor / pow(abs(dist), 5.0);
+       finalColor += luminosity2 * vec4(1.0, 1.0, 0.0, 0.8) / pow(abs(dist), 5.0) / 2.0;
+       finalColor += luminosity3 * textureColor / pow(abs(dist), 5.0);
        // float luminosityToPi = luminosity / 2.0 * M_PI;
        // float luminosity2 = sin(luminosityToPi*1.0)+sin(luminosityToPi*2.0)+cos(luminosityToPi*3.0)+sin(luminosityToPi*4.0);
        //finalColor += (luminosity2+4.0)/8.0 * textureColor / pow(abs(dist), 5.0);
 
-       //finalColor = vec4(oppZ,0.0,0.0,1.0);
+       //finalColor = vec4((luminosity2+1.0)/2.0*0.8+0.2,0.0,0.0,1.0);
      }
    }
    //If we are meant to draw the primitives of the tetrahedron, we simply draw it
